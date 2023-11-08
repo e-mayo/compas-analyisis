ac25876f1bd75d89f0e6b1df38da3ad7b41694aa
Eduardo Mayo <32571404+e-mayo@users.noreply.github.com>
2023-11-08 16:24:50 +0200

    update add colors.py to store the color scheme used in all the plots

---
+++ notebooks/colors.py
@@ -0,0 +1,23 @@
+import matplotlib
+colors = [
+        [0, "#0082C1"], # 
+        [0.5, "#A768A8"], # 
+        [1, "#730B6E"], # 
+            ]
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", colors)
+
+
+heterocycles_colors = {
+    'benzene':'#686868',            # aromatic
+    'cyclobutadiene':'#686868',     # antiaromatic
+    'pyrrole':'#0099FF',            # aromatic
+    'borole':'#00D8C9',             # antiaromatic
+    'furan':'#CF252E',              # aromatic
+    'thiophene':'#FBD55C',          # aromatic
+    'dhdiborinine':'#00746D',       # antiaromatic
+    '14diborinine':'#00948B',       # aromatic 
+    'pyrazine':'#015CAC',           # aromatic
+    'pyridine':'#077AD5',           # aromatic
+    'borinine':'#00B5A9',           # aromatic
+
+}
