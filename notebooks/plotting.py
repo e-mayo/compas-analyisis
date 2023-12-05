2035dc8af5d9c1ffa714afd6299880c6dafedea7
Eduardo Mayo <32571404+e-mayo@users.noreply.github.com>
2023-12-05 12:01:58 +0200

    update create plotting.py with functions

---
+++ notebooks/plotting.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+def plotlineplot_properties_hued_by(dataset, x, props, remove_legend=False, **kwargs):
+    """
+
+    Example:
+    --------
+    >>> plotlineplot_properties_hued_by(df.query('heteroatoms <= 13'),x = 'heterocycles', c='#212121')
+    >>> plotlineplot_properties_hued_by(df.query('heteroatoms <= 13'),x = 'heteroatoms', c='#2064AA')
+    """
+    fig, axs = plt.subplots(2, 3, figsize=(15, 5))
+    axs = axs.flatten()
+    for i, prop in enumerate(props):
+        sns.lineplot(data=dataset, x=x, y=prop, err_style="bars", ax=axs[i], **kwargs)
+        # if remove_legend: axs[i].legend_.remove()
+        remove_legend = True
+        # reove y label
+        axs[i].set_ylabel('')
+    fig.suptitle(f"Grouped by {x}")
+    plt.tight_layout()
+    return fig, axs
